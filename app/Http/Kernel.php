74b191b44d9dab9ff56d77cc20b86adfcb778017
Afeefa-Batool <99643705+Afeefa-Batool@users.noreply.github.com>
2022-04-03 03:02:39 -0700

    product seeder created
    in this part i created dummy products through seeders

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\UserAuth::class,
         \App\Http\Middleware\TrustProxies::class,
-        // \Fruitcake\Cors\HandleCors::class,
+     
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-
-
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
